0d9a2addf48de1018435f19bc3343a06fdf9c90a
Andres F. Silvera <andresfsilverall0109@gmail.com>
2023-09-15 20:40:58 -0500

    tests unitarios agregados

--- Authenticate/views.py
+++ Authenticate/views.py
@@ -52,7 +52,7 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    return render(request, "home.html")
+    return render(request, "navbar.html")
 
 
 def restart_password(request):
